c523083213257405d1784732fbc1c3ff2620a06a
Ojus Soni <77000817+ojus208@users.noreply.github.com>
2024-02-06 00:52:10 +0530

    Update Projects.jsx

--- app/sections/Projects.jsx
+++ app/sections/Projects.jsx
@@ -10,7 +10,7 @@ const Projects = () => {
     const [projectlist, setprojectlist] = useState([])
     useEffect(() => {
         const getporjectlist = async()=>{
-          let res = await fetch('http://localhost:3000/api/projects',{
+          let res = await fetch('api/projects',{
             method: "GET",
             headers: {
               "Content-Type": "application/json",
